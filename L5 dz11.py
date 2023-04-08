0092f422cfed50106a047bab7d9e823118c49531
Dima <lazwar45@gmail.com>
2023-04-08 21:49:22 +0300

    L5 dz11

--- L5 dz11.py
+++ L5 dz11.py
@@ -3,19 +3,23 @@ while True:
     b = float(input("Введите второе число: "))
     sign = input("Введите оператор (+,-,*,/): ")
     if sign == "+":
-        print(a + b)
+        c = a + b
+        print(c)
     elif sign == "-":
-        print(a - b)
+        c = a - b
+        print(c)
     elif sign == "*":
-        print(a * b)
+        c = a * b
+        print(c)
     elif sign == "/":
         if not b:
-            print("На ноль не делится")
-        if b > 0:
-            print(a / b)
+            print("На ноль не делится.")
+        if b != 0:
+            c = a / b
+            print(c)
     else:
         print("Неверный оператор")
-    finish = input("Хотите продолжить работу\nкалькулятора? (y/n): ")
+    finish = input("Продолжить работу? (y/n): ")
     if finish.lower() != "y":
         print("Завершение работы")
         break
