d0e5c245c9a883bb7276bb045ebf6a5657113ff2
Dima <lazwar45@gmail.com>
2023-04-11 19:14:41 +0300

    L5 dz12

--- L5 dz12.py
+++ L5 dz12.py
@@ -2,7 +2,7 @@ import string
 
 s = input('Введите строку: ')
 
-s = s.translate(str.maketrans('', '', string.punctuation()))
+s = s.translate(str.maketrans('', '', string.punctuation))
 s = s.title()
 s = s[:140]
 s = '#' + ''.join(s.split())
