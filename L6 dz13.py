ea31daa4d778b76680e0f69e7cb13d7bd94be4ee
Dima <lazwar45@gmail.com>
2023-04-11 15:31:07 +0300

    Добавлено более простое решение.

--- L6 dz13.py
+++ L6 dz13.py
@@ -7,15 +7,20 @@
 # al_bt = letter_range[letter_range.index(start):letter_range.index(end)+1]
 # print(al_bt)
 
-import string
+# import string
+#
+# letter_range = string.ascii_letters
+# start, end = input("Ввести диапазон букв : ").split('-')
+# if end.islower():
+#     letter_range = string.ascii_letters[string.ascii_lowercase.index
+#                                         (start.lower()):string.ascii_lowercase.index(end.lower()) + 1]
+# else:
+#     letter_range = string.ascii_letters[string.ascii_lowercase.index
+#                                         (start.lower()):
+#                                         len(string.ascii_lowercase) + string.ascii_uppercase.index(end.upper()) + 1]
+# print(letter_range)
 
+import string
 letter_range = string.ascii_letters
 start, end = input("Ввести диапазон букв : ").split('-')
-if end.islower():
-    letter_range = string.ascii_letters[string.ascii_lowercase.index
-                                        (start.lower()):string.ascii_lowercase.index(end.lower()) + 1]
-else:
-    letter_range = string.ascii_letters[string.ascii_lowercase.index
-                                        (start.lower()):
-                                        len(string.ascii_lowercase) + string.ascii_uppercase.index(end.upper()) + 1]
-print(letter_range)
+print(letter_range[letter_range.index(start): letter_range.index(end) + 1])
