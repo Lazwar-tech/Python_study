9bc1b55f5ae1f8a5b5081337a246043d94dc2837
Dima <lazwar45@gmail.com>
2023-04-12 12:02:38 +0300

    Исправил максимальную длину hashtag с 141 до 140.

--- L5 dz12.py
+++ L5 dz12.py
@@ -4,6 +4,9 @@ s = input('Введите строку: ')
 
 s = s.translate(str.maketrans('', '', string.punctuation))
 s = s.title()
-s = s[:140]
 s = '#' + ''.join(s.split())
+s = s[:140]
 print(s)
+
+long = len(s)
+print(long)
